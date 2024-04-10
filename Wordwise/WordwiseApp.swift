e6c994ca474b4849ca2193faa331388c38c39d6f
Oli Kelly <93444456+olikelly00@users.noreply.github.com>
2024-04-10 09:58:40 +0100

    Initial Commit

---
+++ Wordwise/WordwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordwiseApp.swift
+//  Wordwise
+//
+//  Created by Oli Kelly on 10/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
